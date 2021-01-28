7629d680d11dc9f84f89e6f499e56669234328e8
Gulruh2101 <74314629+Gulruh2101@users.noreply.github.com>
2021-01-28 22:38:59 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# change_background_color
+ 
---
+++ javascript.js
@@ -0,0 +1,26 @@
+alert('Assalomu alaykum qadrdonim!🌺')
+const btn0 = document.getElementById('zero');
+const btn1 = document.getElementById('first');
+const btn2 = document.getElementById('second');
+const btn3 = document.getElementById('third');
+const btn4 = document.getElementById('fourth');
+const btn5 = document.getElementById('fifth');
+
+btn0.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = '#880D57';
+})
+btn1.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = '#F77F00';
+})
+btn2.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = '#F97068';
+})
+btn3.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = '#41C83A';
+})
+btn4.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = '#4F4789';
+})
+btn5.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = '#B80D57';
+})
